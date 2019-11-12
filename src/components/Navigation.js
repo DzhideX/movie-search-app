eb8bd5c016f5e9a696becf93b7ceb12a4afaef06
Edvin Dzidic <edvindzidic2000@gmail.com>
2019-11-12 20:51:40 +0100

    Make Navigation a function component that uses React Hooks

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,44 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getMovies } from '../actions/moviesActions';
 import { store } from '../store/configureStore';
 import { connect } from 'react-redux';
 
-export class Navigation extends React.Component {
+export const Navigation = () =>{
 
-    state = {
-        value: '',
-    };
+    const [value, setValue] = useState('');
 
-    updateInput = (e) => {
+    const updateInput = (e) => {
         const value = e.target.value;
-            this.setState(prevState => ({
-                value
-            }));
+        setValue(value);
     };
 
-    handleSubmit = async () => {
-        if(this.state.value){
-            store.dispatch(getMovies(this.state.value));
+    const handleSubmit = async () => {
+        if(value){
+            store.dispatch(getMovies(value));
         }
     }
 
-    handleKeyPress = (target) => {
-        if(this.state.value && target.charCode===13){
-            store.dispatch(getMovies(this.state.value));
+    const handleKeyPress = (target) => {
+        if(value && target.charCode===13){
+            store.dispatch(getMovies(value));
         }
     }
-
-    render(){
+    
         return(
             <div data-test='navigation' className='navigation'>
                 <h3 data-test='navigation-logo' className='navigation-logo'> MovieSearch </h3>
                 <div className='navigation-search'>
-                    <input className='navigation-search__input' placeholder="Search for some movies.." onKeyPress={this.handleKeyPress} value={this.state.value} onChange={this.updateInput}/>
-                    <button className='navigation-search__button' onClick={this.handleSubmit}> Search </button>
+                    <input className='navigation-search__input' placeholder="Search for some movies.." onKeyPress={handleKeyPress} value={value} onChange={updateInput}/>
+                    <button className='navigation-search__button' onClick={handleSubmit}> Search </button>
                 </div>
             </div>
         );
-    };
 };
 
 const mapStateToProps = (state) => ({  movies: state.movies, fetched: state.fetched });
